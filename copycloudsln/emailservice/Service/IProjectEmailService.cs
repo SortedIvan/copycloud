5df8003121134a296c81729bfb0b6485f980db68
Ivan <450754@student.fontys.nl>
2023-05-11 12:17:57 +0200

    remove: deleted redundant emailservice

--- copycloudsln/emailservice/Service/IProjectEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace emailservice.Service
-{
-    public interface IProjectEmailService
-    {
-        Task<bool> SendPersonJoinedEmail(string to, string who, string projectId, string projectName);
-        Task<bool> SendProjectInviteEmail(string receiver, string sender,string inviteToken, string projectName, string projectId);
-    }
-}
